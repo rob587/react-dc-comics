e25e727e9e765957a3bb1c5adbbf6b5152bc530e
rob587 <robertocamm02@yahoo.com>
2025-08-25 16:24:44 +0200

    main completato

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,11 +5,30 @@ const Main = () => {
     <main>
         <div className="main-content">
             <h2>--&gt; Content Goes Here &lt;--</h2>
+
         </div>
         <div className="content-div">
             <div className="imgs-text">
-                <img src="../src/assets/img/card.png" alt="" />
+                <img className="tag" src="../src/assets/img/card.png" alt="" />
+                <h3>Digital Comics</h3>
             </div>
+            <div className="imgs-text">
+                <img className="tag" src="../src/assets/img/merch.png" alt="" />
+                <h3>Digital Comics</h3>
+            </div>
+            <div className="imgs-text">
+                <img className="tag" src="../src/assets/img/sub.png" alt="" />
+                <h3>Digital Comics</h3>
+            </div>
+            <div className="imgs-text">
+                <img className="tag" src="../src/assets/img/shop.png" alt="" />
+                <h3>Digital Comics</h3>
+            </div>
+            <div className="imgs-text">
+                <img className="tag" src="../src/assets/img/power.svg" alt="" />
+                <h3>Digital Comics</h3>
+            </div>
+            
         
         </div>
       
