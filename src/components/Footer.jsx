f7f3482a3b3643243a8830c35ef52741c2ae1103
rob587 <robertocamm02@yahoo.com>
2025-08-26 00:33:11 +0200

    modify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,42 +3,56 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="jumbotron">
+
+            <div className="jumbotron">
         <div className="terms-and-right">
-          
-          <div>
-            <h1 className="title">Dc Comics</h1>
-            <ul className="terms">
-              <li>Characters</li>
-              <li>Comics</li>
-              <li>Movies</li>
-              <li>TV</li>
-              <li>Games</li>
-              <li>Videos</li>
-              <li>News</li>
-            </ul>
+
+          <div className="columns">
+            <div>
+              <h1 className="title">Dc Comics</h1>
+              <ul className="terms">
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+              </ul>
+            </div>
+
+            <div>
+              <h1 className="title">Shop</h1>
+              <ul className="terms">
+                <li>Shop DC</li>
+                <li>Shop Collectibles</li>
+              </ul>
+            </div>
+
+            <div>
+              <h1 className="title">DC</h1>
+              <ul className="terms">
+                <li>Terms Of Use</li>
+                <li>Privacy Policy</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+              </ul>
+            </div>
           </div>
 
-          <div>
-            <h1 className="title">Shop</h1>
-            <ul className="terms">
-              <li>Shop DC</li>
-              <li>Shop Collectibles</li>
-            </ul>
-          </div>
-
-          <div>
-            <h1 className="title">DC</h1>
-            <ul className="terms">
-              <li>Terms Of Use</li>
-              <li>Privacy Policy</li>
-              <li>Jobs</li>
-              <li>Subscriptions</li>
-            </ul>
+        
+          <div className="right-logo">
+            <img src="../src/assets/img/dc-logo-bg.png" alt="DC Logo" />
           </div>
 
         </div>
       </div>
+
+
+      <div className="footer-infos">
+
+      </div>
+
       
     </footer>
   )
