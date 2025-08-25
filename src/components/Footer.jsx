0157d6fcf5286b6ddd8bda2c1450abda4b42cbe4
rob587 <robertocamm02@yahoo.com>
2025-08-26 00:26:53 +0200

    footer in sviluppo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,45 @@
 
 const Footer = () => {
   return (
-    <div>
+    <footer>
+      <div className="jumbotron">
+        <div className="terms-and-right">
+          
+          <div>
+            <h1 className="title">Dc Comics</h1>
+            <ul className="terms">
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>TV</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
+          </div>
+
+          <div>
+            <h1 className="title">Shop</h1>
+            <ul className="terms">
+              <li>Shop DC</li>
+              <li>Shop Collectibles</li>
+            </ul>
+          </div>
+
+          <div>
+            <h1 className="title">DC</h1>
+            <ul className="terms">
+              <li>Terms Of Use</li>
+              <li>Privacy Policy</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
       
-    </div>
+    </footer>
   )
 }
 
